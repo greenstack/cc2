3a0bbc40706abd657858ccc3778040b4ea0178fe
Joseph Newman <paradoxbuilder@gmail.com>
2019-02-21 21:00:37 -0700

    Implemented map loading.

--- world/map/map.lua
+++ world/map/map.lua
@@ -1,15 +1,29 @@
 require "world.map.layer"
+require "world.map.tileset"
 
 Map = {
   Name = "",
   Layers = {},
-  type = "Map"
+  type = "Map",
+  MapWidth,
+  MapHeight,
+  Tileset,
 }
 
-function Map:new(levelName)
-  o = {}
+function Map:new(levelName, tilesetPath, o)
+  o = o or {}
   setmetatable(o, self)
   self.__index = self
-  o.name = levelName
+  o.Name = levelName
+  local levelData = require(levelName)
+  o.MapWidth = levelData.width
+  o.MapHeight = levelData.height
+  o.Layers = {}
+  local layerId = 1
+  for key, layer in ipairs(levelData.layers) do
+    table.insert(o.Layers, Layer:new(layer, layerId))
+    layerId = layerId + 1
+  end
+  o.Tileset = Tileset:new(tilesetPath, levelData.tilewidth, levelData.tileheight)
   return o
 end
--- world/map/tile.lua
+++ world/map/tile.lua
@@ -1,6 +1,6 @@
 Tile = {
   TileId = 0,
-  layer = -1,
+  layer = 5,
   type = "Tile",
 }
 
@@ -11,6 +11,7 @@ function Tile:new(tileId, layer, o)
   o = o or {}
   setmetatable(o, self)
   self.__index = self
+  --print("On layer " .. layer .. ", a tile with id " .. tileId)
   o.TileId = tileId
   o.layer = layer
   return o
