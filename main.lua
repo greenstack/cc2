6345c18aa31bebdfd87fc1c6e8505f698ccd9301
Joseph Newman <paradoxbuilder@gmail.com>
2019-02-21 21:01:26 -0700

    Code to test level loading/camera rendering.

--- main.lua
+++ main.lua
@@ -5,11 +5,26 @@ require "world"
 require "input"
 require "interactions"
 
-require "graphics"
-require "ui"
+require "world.map.map"
+require "graphics.camera"
+--require "graphics"
+--require "ui"
 
-function love.init()
-
+function love.load()
+  map = Map:new("assets/maps/level3_cc1", "assets/img/tiles.png")
+  --print(map.Layers[1]:IsTileEmpty(1, 1))
+  --print(map.Layers[1]:GetTile(1, 1).TileId)
+  
+  camera = Camera:new()
+  camera:SetMap(map)
+  --tileset = Tileset:new("assets/img/tiles.png", 32, 32)
+  --tilesetBatch = love.graphics.newSpriteBatch(tileset.Image, tileset.ImageWidth * tileset.ImageHeight)
+  --for i = 0, 10 - 1  do
+  --  for j = 1, 10 do
+  --    tilesetBatch:add(tileset:GetTile(i * 10 + j), i * 32, j * 32)
+  --  end
+  --end
+  --tilesetBatch:flush()
 end
 
 function love.update(dt)
@@ -20,6 +35,6 @@ function love.update(dt)
 end
 
 function love.draw()
-  graphics:draw(world)
-  ui:draw(world,player) 
+  --love.graphics.draw(tilesetBatch)
+  camera:Draw()
 end
