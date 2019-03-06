c1ee21d539390896df49cf38b04ba736a1271476
Jason <thecooljason@gmail.com>
2019-03-05 18:19:52 -0700

    Create level.lua

---
+++ world/level.lua
@@ -0,0 +1,21 @@
+local weather = require 'weather.weather'
+
+
+function level:generate()
+
+end
+
+
+
+function level:getLevelContactRequirement(level, weather)
+    
+    if level == 1 then
+
+    elseif level == 2 then
+
+    elseif level == 3 then
+
+    end
+
+
+end
