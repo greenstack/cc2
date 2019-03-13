b1213aa5c8a20d075858e668dbd75d4a5ac4470a
Joseph Newman <paradoxbuilder@gmail.com>
2019-03-13 17:28:04 -0600

    Added names to weather.

--- world/weather.lua
+++ world/weather.lua
@@ -1,4 +1,5 @@
 local WeatherPattern = {
+  Name,
   -- Modifies a generated NPC's receptiveness stat.
   ReceptivenessModifier = 0,
   -- Modifies a generated NPC's flirtiness stat.
@@ -7,14 +8,16 @@ local WeatherPattern = {
   SpawnRateModifier = 0,
   -- Decreases how far the player can see
   VisionModifier = 0,
+  Shader,
   type = "WeatherPattern"
 }
 
 -- Creates a new weather pattern.
-function WeatherPattern:new(rMod, fMod, srMod, vMod) 
+function WeatherPattern:new(name, rMod, fMod, srMod, vMod) 
   o = {}
   setmetatable(o, self)
   self.__index = self
+  o.Name = name
   o.ReceptivenessModifier = rMod
   o.FlirtinessModifier = fMod
   o.SpawnRateModifier = srMod
@@ -24,11 +27,19 @@ end
 
 Weather = {
   -- Clear: Nothing special.
-  Clear = WeatherPattern:new(1, 1, 1, 1),
+  Clear = WeatherPattern:new("Clear", 1, 1, 1, 1),
   -- Rainy: Vision is slightly decreased, as are spawn rates.
-  Rainy = WeatherPattern:new(1, 1, .5, .75),
+  Rainy = WeatherPattern:new("Rainy", 1, 1, .5, .75),
   -- Sunny: People are less receptive, more flirtatious, and more likely to spawn.
-  Sunny = WeatherPattern:new(.5, 1.5, 1.5, 1),
+  Sunny = WeatherPattern:new("Sunny", .5, 1.5, 1.5, 1),
   -- Foggy: You can't see as far as you normally can.
-  Foggy = WeatherPattern:new(1, 1, 1, .5)
+  Foggy = WeatherPattern:new("Foggy", 1, 1, 1, .5)
 }
+
+function SetWeatherShaders()
+  Weather.Foggy.Shader = love.graphics.newShader("graphics/shaders/fog.frag")
+  Weather.Foggy.Shader:send("fog_distance", 428)
+  Weather.Foggy.Shader:send("fog_distance_min", 256)
+
+  Weather.Sunny.Shader = love.graphics.newShader("graphics/shaders/sun.frag")
+end
